3f5341f932d5493704ee00a4459f63f133f11463
decker757 <ernesttanjianyu@gmail.com>
2024-12-18 14:32:19 +0800

    Updated DriverAbout

--- meal_share/frontend/src/pages/DriverAbout.jsx
+++ meal_share/frontend/src/pages/DriverAbout.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react';
 import StoreLocator from "../components/StoreLocator";
+import { useLocation, useNavigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <h1>Free Meals Locator</h1>
-      <StoreLocator />
-    </div>
-  );
+function DriverAbout() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const username = location.state?.username || 'User';
+
+    const handleLogout =() => {
+        navigate('/');
+    };
+    return (
+        <div>
+            <h2>Welcome, {username}!</h2>
+            <h1>Free Meals Locator</h1>
+            <StoreLocator/>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    ); 
 }
 
-export default App;
+export default DriverAbout;
